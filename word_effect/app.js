919fcac8768ba8b155075b87ab609dfe896f3f1b
amira-haouet <haouetamira@gmail.com>
2021-12-11 09:53:36 +0100

    inisialize js app

--- word_effect/app.js
+++ word_effect/app.js
@@ -1,7 +1,9 @@
 var classe ={}
 
-classe.opacityIn=[]
+classe.opacityIn=[0,1]
 classe.opacityOut=[]
-classe.durationOut=[]
-classe.durationIn=[]
-classe.delay=[]
+classe.durationOut=900;
+classe.durationIn=700;
+classe.delay=500;
+
+anime.timeline({loop:true})
